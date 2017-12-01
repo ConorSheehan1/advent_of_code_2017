d591c972186cd579e2f20e1e90d783392977dd7c
conor <conor.sheehan@live.ie>
2017-12-01 21:04:33 +0000

    day1 done

--- day1/main.py
+++ day1/main.py
@@ -1,2 +1,16 @@
-def main(data):
-    return data
+def part1(data):
+    to_sum = [0]
+    for i in range(len(data)):
+        if data[i] == data[(i+1)%len(data)]:
+            to_sum.append(data[i])
+    return sum(map(lambda x: int(x), to_sum))
+
+
+def part2(data):
+    to_sum = [0]
+    for i in range(len(data)):
+        # check for integer division?
+        # print(i, (i+(len(data)//2))%len(data))
+        if data[i] == data[(i+(len(data)//2))%len(data)]:
+            to_sum.append(data[i])
+    return sum(map(lambda x: int(x), to_sum))
--- run_day.py
+++ run_day.py
@@ -8,33 +8,35 @@ use this script to load the input file and main function from the module for a g
 log out and dat/time to module folder
 
 usage:
-python run_day.py folder_name, input_file_name
+python run_day.py target_folder, method, input_file_name, write_output
 '''
 
-try:
-    target_folder = sys.argv[1]
-except IndexError:
-    print("Please pass the name of a module to be run")
-    exit()
 
-module = importlib.import_module(target_folder + ".main")
+def main(target_folder, method="part1", input_file_name="input.txt", write_output=False):
+    module = importlib.import_module(target_folder + ".main")
 
-# for using test input
-if len(sys.argv) == 3:
-    file_name = sys.argv[2]
-    write_output = False
-# for running real input
-else:
-    file_name = "input.txt"
-    write_output = True
+    # read the first line of the file and pass it to the main function for the given day
+    with open("{}/{}".format(target_folder, input_file_name)) as input_file:
+        # read lines and remove new line chars
+        data = map(lambda x: x.replace("\n", ""), input_file.readlines())
 
-# read the data file and pass it to the main function for the given day
-with open("{}/{}".format(target_folder, file_name)) as input_file:
-    data = input_file.readlines()
+    # call part 1 or part 2, apply to all lines in input_file
+    output = list(map(getattr(module, method), data))
+    if write_output:
+        output_file_name = "{}/{}_{}.{}".format(target_folder, method, time.strftime("%Y-%m-%d_%H_%M"), "txt")
+        with open(output_file_name, "w") as out_file:
+            out_file.write(str(output))
+    else:
+        print("output:", output)
 
-output = module.main(data)
-if write_output:
-    # with open("{}/{}{}{}".format(target_folder, "output", time.strftime("%Y-%m-%d_%H_%M"), ".txt"), "w") as out_file:
-    #     pass
-else:
-    print(output)
+
+if __name__ == "__main__":
+    try:
+        target_folder = sys.argv[1]
+    except IndexError:
+        print("Please pass the name of a module to be run")
+        exit()
+
+    # unpack additional args and pass to main function
+    main(target_folder, *sys.argv[2:])
+
