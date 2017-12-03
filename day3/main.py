85fe645d6b38f170336a3c108992f43b80c0cc82
conor <conor.sheehan@live.ie>
2017-12-03 21:58:16 +0000

    day3 part1 done but messy and not using run_day.py to load data

--- day3/main.py
+++ day3/main.py
@@ -9,8 +9,7 @@ def is_square(data):
     return True
 
 # question about cleaner way to format format string
-def print_grid(grid, num, height):
-    print("num={}, height={}".format(num, height))
+def print_grid(grid):
     # biggest number is always bottom right
     space_size = len(str(grid[-1][-1])) + 2
     for row in grid:
@@ -18,15 +17,13 @@ def print_grid(grid, num, height):
         output_string = ""
         for number in row:
             output_string += to_format.format(number)
-        print(output_string, row)
+        print(output_string)
 
 def generate_grid(size):
     num, height = 1, 1
     grid = [[1]]
     # always make a square grid
     while num < size:
-        print_grid(grid, num, height)
-
         num += 1
         # if grid is square, add next number to the right of the current bottom right number
         if is_square(grid):
@@ -35,14 +32,12 @@ def generate_grid(size):
             # then fill the new right hand column, don't take into account bottom and top row
             for i in range(2, height+1):
                 num += 1
-                print("!!", -i, num)
                 grid[-i].append(num)
 
         top_row = []
         for i in range(height+1):
             num+=1
             top_row.append(num)
-        print("top row", top_row)
 
         # add the top row (in reverse)
         grid.insert(0, top_row[::-1])
@@ -58,12 +53,29 @@ def generate_grid(size):
         for i in range(height+1):
             num+=1
             bottom_row.append(num)
-        print("bottom row", bottom_row)
 
         grid.append(bottom_row)
         height += 1
-        print_grid(grid, num, height)
     return grid
 
+def grid_location(grid, num):
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j]==num:
+                return i, j
+
+def distance(x1,y1,x2,y2):
+    return abs(x1-x2) + abs(y1-y2)
+
 if __name__ == "__main__":
-    generate_grid(int(input()))
+    user_input = int(input())
+    answer = generate_grid(user_input)
+    print_grid(answer)
+    x1, y1 = grid_location(answer, user_input)
+    x2, y2 = grid_location(answer, 1)
+
+    # 1 is always at the center of the grid, which is the height of the grid divided by 2 (integer division truncated)
+    # print(len(answer)//2)
+
+    print(distance(x1,y1,x2,y2))
+    # input = 368078 answer = 371
