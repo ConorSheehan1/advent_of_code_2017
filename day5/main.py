348a0ddc5e7c21e12757bd069def4da7c0047795
conor <conor.sheehan@live.ie>
2017-12-05 22:02:34 +0000

    start day 5

---
+++ day5/main.py
@@ -0,0 +1,26 @@
+def clean_data(data):
+    # each line in the file contains a list with one number, so return a flat list with all numbers
+    return [int(line) for line in data]
+
+def print_data(i, data):
+    string = "["
+    for index, value in enumerate(data):
+        if index == i:
+            string += "({}),\t".format(value)
+        else:
+            string += "{},\t".format(value)
+    string += "]"
+    print(string)
+
+
+def part1(data):
+    i, count = 0, 0
+    print_data(count, data)
+    while i < len(data):
+        data[i]+=1
+        print(i, data[i], i+data[i])
+        i = data[i] if data[i] >= 0 else i + data[i]
+        count += 1
+        print_data(i, data)
+    return count
+
---
+++ up.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source activate aoc_2017
