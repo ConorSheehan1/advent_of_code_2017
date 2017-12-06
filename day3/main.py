928f8ab6892c20d12a6e16312f562c3f31f165c8
conor <conor.sheehan@live.ie>
2017-12-06 22:53:56 +0000

    day3 part2 working but too slow, don't get traversal order up front

--- day3/main.py
+++ day3/main.py
@@ -61,43 +61,23 @@ class SpiralGrid:
                 if self.grid[i][j]==number:
                     return i, j
 
-class Square:
-    def __init__(self, bottom, top, left, right):
-        self.bottom = bottom
-        self.top = top
-        self.left = left
-        self.right = right
-
-    def get_coordinates(self):
-        answer = []
-        for y in range(self.top, self.bottom+1):
-            for x in range(self.left, self.right+1):
-                answer.append([y,x])
-        return answer
-
-    def move_down(self):
-        self.bottom += 1
-        self.top += 1
-
-    def move_up(self):
-        self.bottom-=1
-        self.top -=1
-
-    def move_left(self):
-        self.left-=1
-        self.right-=1
-
-    def move_right(self):
-        self.left+=1
-        self.right+=1
-
 
 class SpiralSumGrid(SpiralGrid):
-    def spiral_sum(self):
+    def __init__(self, min_num_in_grid):
+        super().__init__(min_num_in_grid)
+        self.trav_order = self.get_traversal_order()
+        self.min_num_in_grid = min_num_in_grid
+
+    def get_traversal_order(self):
         # get the index of each number in the grid
         indexes = [self.get_location(num) for num in range(1, self.grid[-1][-1]+1)]
         # remove any index that returns none because it's not in the grid
-        formatted_indexes = [tup for tup in indexes if tup]
+        return [tup for tup in indexes if tup]
+
+
+    def spiral_sum(self):
+        # formatted_indexes = self.get_traversal_order()
+
         # set the grid to all 0s
         self.grid = [[0]*self.height for _ in range(self.height)]
 
@@ -105,29 +85,23 @@ class SpiralSumGrid(SpiralGrid):
         mid_index = self.height//2 
         self.grid[mid_index][mid_index] = 1
 
-        # print(formatted_indexes)
-
-        # define location of square
-        self.square = Square(mid_index, mid_index-1, mid_index, mid_index+1)
-        print(self.square.get_coordinates())
-
-        for index in formatted_indexes:
-            print(self)
+        for index in self.trav_order:
             y,x = index
-            self.grid[y][x] = self.get_sum(index)
-            # self.move_square(index)
-            # self.grid[y][x] = self.get_square_sum()
-            # if self.get_square_sum() >= 11:
-            #     print("error", self.square.get_coordinates(), index)
+            replacement_num = self.get_sum(index)
+            if self.get_sum(index) > self.min_num_in_grid:
+                return replacement_num
+
+            print(replacement_num, self.min_num_in_grid)
+            self.grid[y][x] = replacement_num
 
     def get_sum(self, index):
+        # get all indexes adjacent to one passed and add their values together
         total = 0
         indexes_to_check = []
         for y in range(index[0]-1, index[0]+2):
             for x in range(index[1]-1, index[1]+2):
                 if y >= 0 and x >= 0:
                     indexes_to_check.append([y,x])
-        print(index, indexes_to_check)
         for l in indexes_to_check:
             try:
                 total += self.grid[l[0]][l[1]]
@@ -136,27 +110,11 @@ class SpiralSumGrid(SpiralGrid):
                 # if index isn't in grid, skip it
         return total
 
-    def move_square(self, index):
-        y,x = index
-        if x > self.square.right:
-            self.square.move_right()
-        if x < self.square.left:
-            self.square.move_left()
-        if y > self.square.bottom:
-            self.square.move_down()
-        if y < self.square.top:
-            self.square.move_up()
+    # def get_location_gt(self, number):
+    #     for index in self.trav_order:
+    #         if self.grid[index[0]][index[1]] > number:
+    #             return index
 
-
-    def get_square_sum(self):
-        answer = 0
-        for index in self.square.get_coordinates():
-            answer += self.grid[index[0]][index[1]]
-        if answer == 22:
-            print("ugh", self.square.get_coordinates())
-            for index in self.square.get_coordinates():
-                print(self.grid[index[0]][index[1]])
-        return answer
 
 
 def clean_data(data):
@@ -172,9 +130,18 @@ def part1(data):
     return distance(x1,y1,x2,y2)
 
 
+def part2(data):
+    print(data)
+    grid = SpiralSumGrid(data)
+    print(grid)
+    return grid.spiral_sum()
+
+
+
 if __name__ == "__main__":
     user_input = int(input("number to search grid for: "))
     grid = SpiralSumGrid(user_input)
+    print(grid, grid.spiral_sum())
     # print(grid)
-    grid.spiral_sum()
-    print(grid)
+
+    # print(grid.get_location_gt(user_input))
