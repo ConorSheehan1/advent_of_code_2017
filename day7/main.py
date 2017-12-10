0f90fa982dda71ff97d95fa073cd5da90915f34b
conor <conor.sheehan@live.ie>
2017-12-10 21:26:24 +0000

    day7 part2 finally done

--- day7/main.py
+++ day7/main.py
@@ -66,42 +66,69 @@ def part1(data):
             # return node
             return node
 
-def get_weight(node_dict):
-    # make copy of dict so that order in which weights are calculated for nodes doesn't matter
-    new_weights = deepcopy(node_dict)
-    # for each node, the weight is itself plus the weight of all its children
-    for node in node_dict.keys():
-        weight = node_dict[node]["weight"]
-        stack = node_dict[node]["children"]
-        # print("weight", weight)
-        while stack:
-            # print("stack", stack)
-            next_node = stack.pop()
-            # print("next weight", node_dict[next_node]["weight"])
-            weight += node_dict[next_node]["weight"]
-            children = node_dict[next_node]["children"]
-            if children:
-                for child in children:
-                    stack.append(child)
-        new_weights[node]["weight"] = weight
-    return new_weights
+def get_root(data):
+    children = []
+    for node in data.keys():
+        [children.append(child) for child in data[node]["children"]]
+    return [node for node in data.keys() if node not in children]
+
+def get_order(data, root):
+    # breadth first traverse the dictionary of nodes and return traversal order (for bottom of graph up)
+    path = [root]
+    queue = [root]
+    while queue:
+        current = queue.pop()
+        children = data[current]["children"]
+        if children:
+            [queue.insert(0, child) for child in children]
+            [path.insert(0, child) for child in children]
+    return path
+
+def get_weight(data, node):
+    data[node]["weight"] += sum(data[child]["weight"] for child in data[node]["children"])
 
 
 def part2(data):
-    data = get_weight(data)
+    # print(get_root(data))
+    # root should always be single value, if not wrong number of args will throw exception
+    original_weight = deepcopy(data)
+    order = get_order(data, *get_root(data))
+    for node in order:
+        get_weight(data, node)
+
+    # # print(data)
+    # weights = [data[node]["weight"] for node in order]
+    # # print(weights)
+    # # create sliding window three spaces wide
+    # # the first weight that is different from the weight before and after it is the problem
+    # for first, second, third in zip(weights, weights[1:], weights[2:]):
+    #     # print(first, second, third)
+    #     # pass
+    #     if first != second and second != third:
+    #         # return first
+    #         print(first, second, third)
+
+    # get the first node whos children don't all have the same weight
+    for node in order:
+        children =  data[node]["children"]
+        if children:
+            first_weight = data[children[0]]["weight"]
+            for child in children:
+                if data[child]["weight"] != first_weight:
+                    child_dict = {c:data[c]["weight"] for c in children}
+                    print(child_dict)
+                    # get the child with the most weight
+                    max_child = max(child_dict, key=child_dict.get)
+                    min_child = min(child_dict, key=child_dict.get)
+                    # return the origin weight of the heaviest child 
+                    # minus the difference between its overall weight and siblings overal weight
+                    return original_weight[max_child]["weight"] - (data[max_child]["weight"] - data[min_child]["weight"])
+
     # print(data["ugml"]["weight"] == 251)
     # print(data["padx"]["weight"] == 243)
     # print(data["fwft"]["weight"] == 243)
 
 
-    for node in data.keys():
-        children = data[node]["children"] 
-        # print(children)
-        if children:
-            weights = [data[child]["weight"] for child in children]
-            if len(set(weights)) > 1:
-                print("weights", weights, children)
-                # return most common weight from uneven group of weights
-                # return max(weights, key=weights.count)
 
 # 2800 too high
+# 2607 too high
