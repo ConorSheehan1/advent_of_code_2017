67dea8ef0a82e0e38cd72c3d5f53335f0c5a8504
conor <conor.sheehan@live.ie>
2017-12-12 22:06:39 +0000

    day09 done

--- day09/main.py
+++ day09/main.py
@@ -4,51 +4,71 @@ def clean_data(data):
         if line[0] != "#":
             return line
 
-# garbage start with < and ends with >
-# anything after ! in garbage is ignored
-
-# remove anything between < and > preceded by a !
 def remove_escaped_data(data):
-    garbage = False
-    i = 0
-    while i < len(data):
-        char = data[i]
-
-        # remove ! and char after it if currently in garbage
-        if char == "!" and garbage:
-            data = data[:i] + data[i+2:]
-            i += 2
+    copy = ""
+    skip = 0
+    for char in data:
+        if char == "!" and skip == 0:
+            skip = 2
+        if skip > 0:
+            skip -= 1
             continue
-        if char == "<" and not garbage:
-            garbage = True
-        if char == ">":
-            garbage = False
-        i += 1
-    return data
+        if char != "!" and skip == 0:
+            copy += char
+    return copy
 
 # remove anything between < and >
-def remove_garbage(data):
+def remove_garbage(data, keep_tag=False):
     garbage = False
-    i = 0
-    while i < len(data):
-        char = data[i]
-        if char == "<" and not garbage:
+    copy = ""
+    for char in data:
+        if char == "<":
             garbage = True
-            to_remove = 0
-            while garbage:
-                if data[i+to_remove] == ">":
-                    garbage=False
-                    # keep the surround < and >, remove rest
-                    data = data[:i+1] + data[i+to_remove:]
-                    break
-                else:
-                    to_remove += 1
-        i += 1
-    return data
+        # use continue top skip the step adding the current character to copy
+        # since the current char is the closing tag for garbage
+        if char == ">":
+            garbage = False
+            continue
+        if not garbage:
+            copy += char
+    return copy
 
 def count_groups(data):
-    pass
+    score = 0
+    level = 0
+    for char in data:
+        if char == "{":
+            level += 1
+            score += level
+        if char == "}":
+            level -= 1
+    return score
+
 
 def part1(data):
-    return remove_garbage(remove_escaped_data(data))
+    data = remove_escaped_data(data)
+    data = remove_garbage(data)
+    return count_groups(data)
+
+def part2(data):
+    formatted = remove_escaped_data(data)
+    no_garbage = remove_garbage(formatted)
+    number_of_garbage_tags = formatted.count(">") * 2
+    return len(formatted) - len(no_garbage) - number_of_garbage_tags
+    # all non-escaped data within garbage tags is the same as the difference between 
+    # all non-escaped data and all non-garbage data 
+    # (minus the garbage tags themselves since they don't count as data within garbage tags) 
+
+    # 18429 too high
+    # 10167 too high
+
+
+if __name__ == "__main__":
+    for k,v in {
+        "{}":1 ,"{{{}}}":6, "{{},{}}":5, "{{{},{},{{}}}}":16, "{<a>,<a>,<a>,<a>}":1,
+        "{{<ab>},{<ab>},{<ab>},{<ab>}}":9, '{{<!!>},{<!!>},{<!!>},{<!!>}}':9,
+        "{{<a!>},{<a!>},{<a!>},{<ab>}}":3
+    }.items():
+        answer = part1(k)
+        print(answer== v, answer, v, "\n\n")
     
