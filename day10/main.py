a85d6979b4dc8e6ba3802715317b12ea3a852db8
conor <conor.sheehan@live.ie>
2017-12-19 22:40:18 +0000

    day10 part2 done, refactor to use part1 later

--- day10/main.py
+++ day10/main.py
@@ -39,26 +39,50 @@ def part1(data, part2=False, i=0, skip=0):
         return knot, i, skip
     return knot[0]*knot[1]
 
+# def part2(data):
+#     data=[1,2,3]
+#     string_data = ",".join(map(str, data))
+#     print(string_data)
+#     bytes_data = [ord(char) for char in string_data]
+#     bytes_data.extend([17, 31, 73, 47, 23])
+#     # bytes_data = [3, 4, 1, 5, 17, 31, 73, 47, 23]
+#     i, skip = 0, 0
+#     for round in range(64):
+#         sparse_hash, i, skip = part1(bytes_data, part2=True, i=i, skip=skip)
+#     print(i, skip, "\n\n")
+#     dense_hash = []
+#     chunk_size = 16
+#     # print(sparse_hash)
+#     for i in range(0, len(sparse_hash), chunk_size):
+#         # xor each 16 bit chunk and add to the dense hash
+#         dense_hash.append(eval("^".join(map(str, sparse_hash[i:i+chunk_size]))))
+#     print(dense_hash)
+#     return "".join([to_hex(num) for num in dense_hash])
+
 def part2(data):
+
+    size = 256
+    # data=[1,2,3]
     string_data = ",".join(map(str, data))
-    print(string_data)
     bytes_data = [ord(char) for char in string_data]
     bytes_data.extend([17, 31, 73, 47, 23])
     # bytes_data = [3, 4, 1, 5, 17, 31, 73, 47, 23]
     i, skip = 0, 0
+    knot = list(range(size))
     for round in range(64):
-        print(bytes_data)
-        if round <= 1:
-            print("round", i, skip)
-        sparse_hash, i, skip = part1(bytes_data, part2=True, i=i, skip=skip)
-    print(i, skip, "\n\n")
+        for length in bytes_data:
+            if length > size:
+                continue
+            reverse_selection(knot, i, length)
+            i += length + skip
+            skip += 1
+    sparse_hash = knot
     dense_hash = []
     chunk_size = 16
     # print(sparse_hash)
     for i in range(0, len(sparse_hash), chunk_size):
         # xor each 16 bit chunk and add to the dense hash
         dense_hash.append(eval("^".join(map(str, sparse_hash[i:i+chunk_size]))))
-    print(dense_hash)
     return "".join([to_hex(num) for num in dense_hash])
 
 
