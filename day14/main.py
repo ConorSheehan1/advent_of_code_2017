7c70d315583dce24f853c623d5b1382e0339eb24
conor <conor.sheehan@live.ie>
2017-12-24 20:53:41 +0000

    day14 part2 done

--- day14/main.py
+++ day14/main.py
@@ -32,41 +32,28 @@ def part1(data, part2=False):
 #8180 too low
 #8188 too low
 
-# def dfs(grid, i, j, num_regions):
-#     print(i, j)
-#     # if the index is out of bound return None
-#     if not (0 <= i < 128) or not (0 <= j < 128):
-#         return 
-#     if grid[i][j] == "1":
-#         grid[i] = "{}{}{}".format(grid[i][:j], num_regions+2, grid[i][j+1:])
-#     else:
-#         return
-#     # below
-#     dfs(grid, i+1, j, num_regions)
-#     # left
-#     dfs(grid, i, j-1, num_regions)
-#     # right
-#     dfs(grid, i, j+1, num_regions)
-#     return True
-
-# def create_group(grid, i, j, num_regions):
-#     for row in grid[i:]:
-#         current = row[j]
-#         if current == "1":
-#             left = row[j-1]
-#             right = row[j+1]
-#             below = grid[i+1][j]
-
-
+def get_region(grid, stack, seen):
+    new_region = 0
+    while stack:
+        i, j = stack.pop()
+        try:
+            if grid[i][j] == "1" and (i, j) not in seen:
+                new_region = 1
+                seen.add((i,j))
+                for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
+                    if 0 <= x < 128 and 0 <= y < 128 and grid[x][y] == "1" and (x,y) not in seen:
+                        stack.append((x,y))
+        except Exception as e:
+            print("err", i, j)
+            raise e
+    return new_region
 
 def part2(data):
     grid = part1(data, part2=True)
+    seen = set()
     regions = 0
-    # there are zeros and ones already, so anything above one will be a region
-    for i, row in enumerate(grid):
-        for j, char in enumerate(row):
-            if char == "1":
-                # if a new group was created, increment regions
-                if dfs(grid, i, j, regions):
-                    regions += 1
+    grid = list(map(list, grid))
+    for i in range(128):
+        for j in range(128):
+            regions += get_region(grid, [(i, j)], seen)
     return regions
