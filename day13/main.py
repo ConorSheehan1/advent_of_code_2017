5003809da257e8167e368b730ae28ad4e115585c
conor <conor.sheehan@live.ie>
2017-12-25 23:38:23 +0000

    day13 part2

--- day13/main.py
+++ day13/main.py
@@ -5,6 +5,13 @@ def clean_data(data):
         d[int(k)] = {"pos":0,"range":int(v), "up":True}
     return d
 
+# def clean_data(data):
+#     d = {}
+#     for line in data:
+#         k,v = line.split(": ")
+#         d[int(k)] = int(v)
+#     return d
+
 # def print_data(data):
 #     # lines is equivilant to highest range
 #     top_line = "\t".join(list(map(str, range(max(data.keys())+1))))
@@ -32,6 +39,16 @@ def reset_scanners(data, offset):
         increment_scanners(data)
     # print(data)
 
+def set_scanners(data, time):
+    for i in data.keys():
+        height = data[i]["range"]
+        offset = time % ((height - 1) * 2)
+
+        if offset > height - 1:
+            data[i]["pos"] =  2 * (height - 1) - offset
+        else:
+            data[i]["pos"] = offset
+
 def part1(data):
     caught = []
     last_position = max(data.keys())
@@ -44,21 +61,22 @@ def part1(data):
     return sum(caught)
 
 def part2(data):
-    start = 26900
+    start = 0
     last_position = max(data.keys())
     while True:
-        start +=1
         # print(start)
-        reset_scanners(data, start)
-        # print(data)
+        set_scanners(data, start)
         for i in range(last_position+1):
+            set_scanners(data, start+i)
             if i in data.keys():
                 if data[i]["pos"] == 0:
-                    print("caught", start)
+                    # print("caught", start)
                     break
             # print("not caught", i)
-            increment_scanners(data)
         else:
-            return start    
+            return start 
+        start +=1 
 
 # 10366 too low
+# 26900 too low
+# 3876272 answer
