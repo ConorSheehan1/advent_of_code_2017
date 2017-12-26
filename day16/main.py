5e345ba672fb6ccccfd99d581303b5f7c77412cc
conor <conor.sheehan@live.ie>
2017-12-26 21:54:51 +0000

    day16 part2

--- day16/main.py
+++ day16/main.py
@@ -7,32 +7,33 @@ def clean_data(data):
     return instructions
 
 def part1(instructions, letters=[c for c in string.ascii_lowercase[:16]]):
-    # # test input
-    # letters = [c for c in string.ascii_lowercase[:5]]
     for instruction in instructions:
         if instruction[0] == "s":
             # move n letters to front of list
             num = int(instruction[1:])
             letters = letters[-num:] + letters[:len(letters)-num]
-        if instruction[0] == "x":
+        elif instruction[0] == "x":
             # swap two letters at position xa/b
             a,b = list(map(int, instruction[1:].split("/")))
             letters[a], letters[b] = letters[b], letters[a]   
-        if instruction[0] == "p":
+        elif instruction[0] == "p":
             namea, nameb = instruction[1:].split("/")
-            indexa = letters.index(namea)
-            indexb = letters.index(nameb)
-            letters[indexa], letters[indexb] =  letters[indexb], letters[indexa]
+            a = letters.index(namea)
+            b = letters.index(nameb)
+            letters[a], letters[b] = letters[b], letters[a]
     return "".join(letters)
+
 
 def part2(instructions):
-    # l = 0
-    letters = [c for c in string.ascii_lowercase[:16]]
-    for i in range(1000000000):
-        # if len(str(i)) > l:
-        #     print(i)
-        #     l+=1
-        tmp = part1(instructions, letters)
-        letters = [c for c in tmp]
-    return "".join(letters)
-
+    count = 0
+    letters = string.ascii_lowercase[:16]
+    seen = [letters[:]]
+    while True:
+        tmp = part1(instructions, [v for v in letters], part2=True)
+        letters = tmp
+        # break as soon as cycle starts
+        if letters in seen:
+            break
+        seen.append(letters)
+        count += 1
+    return seen[1000000000%len(seen)]
