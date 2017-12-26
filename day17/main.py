99d29ac8c4efcb0f36547532ebd9ac5a041191f4
conor <conor.sheehan@live.ie>
2017-12-26 22:37:07 +0000

    day17 part2

--- day17/main.py
+++ day17/main.py
@@ -1,24 +1,37 @@
 def clean_data(data):
-  return int(data[0])
+    return int(data[0])
 
 def part1(data, part2=False, limit=2018):
-  step_size = data
-  # step_size = 3
-  circular_buffer = [0]
-  current_position = 0
-  # 1 up to 2017
-  for i in range(1, limit):
-    current_position += step_size
-    current_position = current_position % len(circular_buffer)
-    current_position += 1
-    circular_buffer.insert(current_position, i)
-    # current position DOES NOT become the inserted value
-    # current_position = i
-  if part2:
-    return circular_buffer
-  return circular_buffer[current_position+1%len(circular_buffer)]
+    step_size = data
+    # step_size = 3
+    circular_buffer = [0]
+    current_position = 0
+    # 1 up to 2017
+    for i in range(1, limit):
+        current_position += step_size
+        current_position = current_position % len(circular_buffer)
+        current_position += 1
+        circular_buffer.insert(current_position, i)
+        # current position DOES NOT become the inserted value
+        # current_position = i
+    if part2:
+        return circular_buffer
+    return circular_buffer[current_position+1%len(circular_buffer)]
+
 
 def part2(data):
-  circular_buffer = part1(data, part2=True, limit=50000000)
-  index = circular_buffer.index(0)
-  return circular_buffer[index+1]
+    step_size = data
+    current = 0
+    after_zero = 0
+    for i in range(1, 50000000):
+        # i is the same as length because array increases one element is inserted each iteration
+        current += step_size
+        current = current% i
+        current += 1
+        if (current == 1):
+            after_zero = i
+    return after_zero
+
+# 1898340 too low
+# 1898341 is right -_-
+# 43292861 too high
